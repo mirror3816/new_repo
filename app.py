03108c538ddd2286df5b2dce6a8c28a0edf8095d
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-11-23 16:08:54 +0900

    add page

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import BytesIO
+import base64
+
+app = Flask(__name__)
+
+# CSV 파일 읽기
+file_path = 'cox-violent-parsed_filt_usable.csv'
+df = pd.read_csv(file_path)
+
+# 성별에 따른 평균 나이 시각화
+@app.route('/average_age_by_gender')
+def average_age_by_gender():
+    plt.figure(figsize=(8, 6))
+    sns.barplot(x='sex', y='age', data=df)
+    plt.title('Average Age by Gender')
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    return render_template('plot.html', plot_url=plot_url)
+
+# 인종에 따른 범죄 기록 수 시각화
+@app.route('/number_of_offenses_by_race')
+def number_of_offenses_by_race():
+    plt.figure(figsize=(8, 6))
+    sns.countplot(x='race', data=df)
+    plt.title('Number of Offenses by Race')
+    img = BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    return render_template('plot.html', plot_url=plot_url)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=1234)
